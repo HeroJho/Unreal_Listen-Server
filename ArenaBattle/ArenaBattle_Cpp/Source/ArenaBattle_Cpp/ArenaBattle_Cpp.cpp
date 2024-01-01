855c557c61152fc1fc60e4fca5fa57cb115b7f4e
HeroJho <theherojho@naver.com>
2024-01-01 13:13:39 +0900

    Init

---
+++ ArenaBattle/ArenaBattle_Cpp/Source/ArenaBattle_Cpp/ArenaBattle_Cpp.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "ArenaBattle_Cpp.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, ArenaBattle_Cpp, "ArenaBattle_Cpp" );
